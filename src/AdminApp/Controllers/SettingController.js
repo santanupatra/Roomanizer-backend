de96260fecdd87732683f6e73eb7a8800b3ff904
ARPIT KUMAR <51833509+arpit20kumar@users.noreply.github.com>
2020-09-11 17:52:49 +0530

    done

--- src/AdminApp/Controllers/SettingController.js
+++ src/AdminApp/Controllers/SettingController.js
@@ -1,27 +1,38 @@
 import config from '../../../config/config';
 import Setting from '../../Models/Setting';
 
-const add = (req, res) => {
+/**
+ * editSetting
+ * return JSON
+ */
+
+
+const editSetting = (req, res) => {
    console.log("add")
     try {
         let allData = req.body;
         const addData = new Setting(allData).save();
         res.status(200).json({msg:"Successfully added"});
-        console.log("add1")
+        
     } catch (err) {
         console.log("Error => ",err.message);
         res.status(500).json({ message:"Something went wrong" });
-        console.log("add2")
+        
     }
 }
 
-const details = async(req, res) => {
-    console.log("hi")
+/**
+ * editSetting
+ * return JSON
+ */
+
+const editSetting = async(req, res) => {
+    console.log("no edit")
     try {
         const getData = await Setting.findById(
             { _id: config.SETTING_PRIMARY_ID}
         );
-        console.log("hello")
+        
         res.status(200).json({data:getData});
     } catch (err) {
         console.log("Error => ",err.message);
@@ -29,7 +40,11 @@ const details = async(req, res) => {
     }
 }
 
-const edit = async(req, res) => {
+/**
+ * editSetting
+ * return JSON
+ */
+const editSetting = async(req, res) => {
     try {
         let allData = req.body;
             if (req.file) {
@@ -53,17 +68,10 @@ const edit = async(req, res) => {
                 res.status(200).json({ msg: "Settings updated successfully" });
             }
         }
-    //     const add = await Setting.findByIdAndUpdate(
-    //         { _id: config.SETTING_PRIMARY_ID},
-    //         {
-    //             $set: allData
-    //         }
-    //     );
-    //     res.status(200).json({msg:"Successfully updated"});
-    //}  
+      
      catch (err) {
         console.log("Error => ",err.message);
         res.status(500).json({ message:"Something went wrong" });
     }
 }
-export default { add, edit, details }
+export default { addSetting, editSetting, detailsSetting }
