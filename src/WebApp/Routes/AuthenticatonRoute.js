6ffd0477111e4c57672fd0fc004a45bd924e938e
mukeshthakur123 <nits.mmukesh@gmail.com>
2020-09-28 06:01:21 +0530

    forget backend

--- src/WebApp/Routes/AuthenticatonRoute.js
+++ src/WebApp/Routes/AuthenticatonRoute.js
@@ -9,4 +9,6 @@ router.post('/Login', AuthenticationController.login);
 router.get('/profileDetails/:userId',Authentication,AuthenticationController.getProfile);
 router.put('/admin/:adminId', upload.uploadUserImage,AuthenticationController.updateProfile);
 router.put('/admin/change-password/:adminId', AuthenticationController.changePassword);
+router.post('/web/forgotPassword', AuthenticationController.forgotPassword);
+router.put('/web/updatePassword', AuthenticationController.updatePassword);
 export default router;
