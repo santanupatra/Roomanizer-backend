256afb30c5d3d199bb9600a330f07dce78ac698f
diganta99 <69569188+diganta99@users.noreply.github.com>
2020-10-01 11:02:44 +0530

    Edit profile

--- src/Models/User.js
+++ src/Models/User.js
@@ -18,7 +18,7 @@ const schema = new Schema({
         required: true
     },
     dateOfBirth: {
-        type: String, 
+        type: String 
     },                   
     phoneNumber: {
         type: String
@@ -29,7 +29,13 @@ const schema = new Schema({
     gender: {
         type: String,
         enum: ['Male','Female','Other']
-    },                 
+    },              
+    occupation: {
+        type: String,
+    },
+    age: {
+        type: Number
+    },
     profilePicture: {
         type: String, 
         default: "/image/user.png"
@@ -41,10 +47,10 @@ const schema = new Schema({
         type: String
     },
     readyToMove: {
-        type: String, 
+        type: Date, 
     },  
     houseRules: {
-        type: String, 
+        type: [], 
     },
     otp: {
         type: String
