329798df3e99aaa657c7884308e6e64118355e73
mukeshthakur123 <nits.mmukesh@gmail.com>
2020-10-05 14:33:22 +0530

    House and Aminities API

--- src/AdminApp/Controllers/ContactController.js
+++ src/AdminApp/Controllers/ContactController.js
@@ -7,12 +7,44 @@ import ContactUs from '../../Models/ContactUs';
  */
 
 const listAll= async(req, res) => {
-    try {
-        const contact = await ContactUs.find({ isDeleted: false });
-        res.status(200).json({data:contact});
-    } catch (err) {
-        console.log('Error => ',err.message);
-        res.status(500).json({msg:"Something went wrong"});
-    }
+    // if(req.query.keyword == null || req.query.page == null){
+    //     return res.status(400).send({ack:1, message:"Parameter missing..."})
+    // }
+   try {
+       let keyword = req.query.keyword;
+       let limit = 10;
+       let page = req.query.page;
+       var skip = (limit*page);
+       const list = await ContactUs.find({
+         
+           isDeleted: false,
+         
+           $or: [
+               { name: { $regex: keyword, $options: 'm' } }
+           ]
+       })
+       .skip(skip)
+       .limit(limit)
+       .sort({ createdDate: 'DESC' });
+       const listAll = await ContactUs.find({
+          
+           isDeleted: false,
+           $or: [
+               { name: { $regex: keyword, $options: 'm' } },
+               { email: { $regex: keyword, $options: 'm' } }
+           ]
+       }).countDocuments();
+       const AllData = {
+           'list': list,
+           'count': listAll,
+           'limit': limit
+       };
+       res.status(200).json({data: AllData});
+   } catch (err) {
+       console.log("Error => ",err.message);
+       res.status(500).json({msg:"Something went wrong."});
+   }
+
+
 }
 export default { listAll}
