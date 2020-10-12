d67c5465c20292f6e612dc66d8ee484948e52628
ARPIT KUMAR <51833509+arpit20kumar@users.noreply.github.com>
2020-10-12 10:08:53 +0530

    API for  image aminities

--- src/AdminApp/Routes/AminitiesRoute.js
+++ src/AdminApp/Routes/AminitiesRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import AminitiesController from '../Controllers/AminitiesController';
+import upload from '../../../config/FileUpload';
+
 // intializing express router
 const router = express.Router();
 
-router.post('/aminities', AminitiesController.createAminities);
+router.post('/aminities',upload.uploadAminitiesImage, AminitiesController.createAminities);
 router.get('/aminities', AminitiesController.listAminitiesies);
 //router.get('/dashboard', UserController.activeUsers);
 router.get('/aminities/:aminitiesId', AminitiesController.listAminities);
-router.put('/aminities/:aminitiesId', AminitiesController.updateAminities);
+router.put('/aminities/:aminitiesId',upload.uploadAminitiesImage, AminitiesController.updateAminities);
 router.delete('/aminities/:aminitiesId', AminitiesController.deleteAminities);
 export default router;
