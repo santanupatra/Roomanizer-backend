c487370857c978562448c3137f7cad239063af7b
mukeshthakur123 <nits.mmukesh@gmail.com>
2020-10-20 15:27:31 +0530

    favroite room dynamic and listing

--- src/WebApp/Routes/FavoriteRoute.js
+++ src/WebApp/Routes/FavoriteRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 import FavoriteController from '../Controllers/FavoriteController';
 import Authorization from '../../../middleware/isAuth';
 router.post('/fav',Authorization,FavoriteController.addfavorite);
-router.get('/favRoomList/:loginUserId',Authorization,FavoriteController.favRoomList);
+router.get('/favRoomList/:loginUserId',FavoriteController.favRoomList);
 router.get('/fav/:loginUserId',FavoriteController.favRoomMateList);
 export default router;
